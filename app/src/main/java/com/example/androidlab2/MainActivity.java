50a6c2f352ff0e552245cebb3414ea318dafdcf8
griml <grimlock.pheornix@bk.ru>
2024-03-27 01:51:25 +0600

    Добавление фрагмента в контейнер

--- app/src/main/java/com/example/androidlab2/MainActivity.java
+++ app/src/main/java/com/example/androidlab2/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.androidlab2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        BlankFragment1 fragment = new BlankFragment1();
+        fragmentTransaction.replace(R.id.placeholder, fragment);
+        fragmentTransaction.commit();
     }
 }
